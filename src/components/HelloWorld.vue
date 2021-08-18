e102eebb5bc89c8fef21493d38f2c780036c287e
tjegham <jeghamseifeddine@gmail.com>
2021-08-18 16:33:05 +0200

    feat(core): static application running in docker

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <div style="padding:40px;">
+      <h1>
+        Hello from TazMania
+      </h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
